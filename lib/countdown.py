88c0497a69b3817fbc58084a0c3a72d767d7ce60
Christian Hernandez <chernandez148@DESKTOP-3F6UKNT.localdomain>
2023-03-18 13:06:03 -0700

    updated

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,21 @@
 # your code goes here!
+import time
+
+def countdown(count):
+    while count > 0:
+        print(f"{count} SECOND(S)!")
+        count -= 1
+        if count == 0:
+            print("HAPPY NEW YEAR!")
+
+countdown(10)
+
+def countdown_with_sleep(count):
+    while count > 0:
+        print(f"{count} SECOND(S)!")
+        count -= 1    
+        time.sleep(1)
+        
+        if count == 0:
+            print("HAPPY NEW YEAR!")
+countdown_with_sleep(10)
